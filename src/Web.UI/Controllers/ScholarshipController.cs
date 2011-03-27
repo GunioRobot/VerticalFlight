06ea9349f1810cb83c59d3e9cced8f02d2fe73ab
unknown <aelandas@.cs.myharris.net>
2011-03-26 19:10:34 -0500

    scholarship chooser development

---
+++ src/Web.UI/Controllers/ScholarshipController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VerticalFlight.Core.Data;
+using VerticalFlight.Core.Domain;
+
+namespace Web.UI.Controllers
+{
+    public class ScholarshipController : Controller
+    {
+        readonly Repository _repository;
+
+        public ScholarshipController(Repository repository)
+        {
+            _repository = repository;
+        }
+
+        //
+        // GET: /Scholarship/
+        public ActionResult Apply()
+        {
+            ViewBag.Message = "Select a scholarship from below to start the application process.";
+            ScholarshipInfo scholarshipInfo = new ScholarshipInfo();
+            return View();
+        }
+
+    }
+}
